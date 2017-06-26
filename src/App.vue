e5fd1278015093510a4773af364050fae45ca474
gaoliangliang <gao_liang0901@163.com>
2017-06-27 07:49:07 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<v-header></v-header>
+</template>
+
+<script>
+import header from './components/header/header.vue';
+export default {
+  components: {
+    'v-header': header
+  }
+};
+window.addEventListener('orientationchange', setRem);
+window.addEventListener('resize', setRem);
+setRem();
+function setRem() {
+  var html = document.querySelector('html');
+  var width = html.getBoundingClientRect().width;
+  html.style.fontSize = width / 15 + 'px';
+}
+</script>
+
+<style>
+@import "index/css/reset.css";
+@import "index/css/page.css";
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,94 @@
+<template>
+<div>
+<header>
+ <div class="header-background">
+   <div class="header">
+     <div class="header-logo"></div>
+     <ul>
+       <li><span>粥品香坊(大运村)</span></li>
+       <li>蜂鸟专送/38分钟送达</li>
+       <li><span>在现支付满28减5,满50减10</span><span>5     &nbsp;></span></li>
+     </ul>
+   </div>
+   <div class="bulletin">
+     <img src="../../index/img/bulletin@2x.png" alt="">
+     <span>粥品香坊坷拉三季稻赶快来就看了撒都经历过弗兰克旧时代风格就咯卡死了得了几分了空间的酸辣凉粉尽力</span>
+     <span>></span>
+   </div>
+ </div>
+</header>
+<div class="content">
+    <ul>
+      <li>商品</li>
+      <li>评价</li>
+      <li>商家</li>
+    </ul>
+</div>
+  <div class="class">
+    <div class="swiper-left">
+      <div class="goods-left">
+        <div><span>热销榜</span></div>
+        <div class="active"><span class="line"><img src="../../index/img/special_3@2x.png" alt="">单人特色套餐</span></div>
+        <div><span>特色粥品</span></div>
+        <div><span>精选热菜</span></div>
+        <div><span>爽口凉菜</span></div>
+        <div><span>半成品</span></div>
+        <div><span>饭类</span></div>
+        <div><span>面类</span></div>
+      </div>
+    </div>
+    <div class="swiper-right">
+      <div class="nav">
+        <div>单人特色套餐</div>
+      </div>
+      <div class="message">
+        <div class="message-main">
+          <div class="message-logo"></div>
+          <ul>
+            <li>皮蛋瘦肉粥配包子套餐</li>
+            <li>咸粥</li>
+            <li><span>月售1132份</span><span>好评率100%</span></li>
+            <li><span>￥24</span><span>￥28</span><span>-</span><span>1</span><span>+</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-right">
+      <div class="nav">
+        <div>单人特色套餐</div>
+      </div>
+      <div class="message">
+        <div class="message-main">
+          <div class="message-logo"></div>
+          <ul>
+            <li>皮蛋瘦肉粥配包子套餐</li>
+            <li>咸粥</li>
+            <li><span>月售1132份</span><span>好评率100%</span></li>
+            <li><span>￥24</span><span>￥28</span><span>-</span><span>1</span><span>+</span></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="font"><i class="icon iconfont">&#xe600;</i></div>
+  </div>
+</div>
+
+</template>
+
+<script>
+    export default {
+
+    };
+</script>
+
+<style >
+  @font-face {font-family: "iconfont";
+    src: url('../../index/font/iconfont.eot'); /* IE9*/
+    src: url('../../index/font/iconfont.eot#iefix') format('embedded-opentype'), /* IE6-IE8 */
+    url('../../index/font/iconfont.woff') format('woff'), /* chrome, firefox */
+    url('../../index/font/iconfont.ttf') format('truetype'), /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
+    url('../../index/font/iconfont.svg#iconfont') format('svg'); /* iOS 4.1- */
+  }
+</style>
